864f282b1be69109b2b2e9e3e68c691b7c1217cd
zusi <41606073+zusizusi@users.noreply.github.com>
2020-03-15 14:52:55 +0900

    サーボの動作を組み込んだ

--- nfcservo.py
+++ nfcservo.py
@@ -9,6 +9,8 @@ import time
 import sqlite3
 import datetime
 import time
+import threading
+import ctypes
 
 
 ###### NFC readerの設定 ######
@@ -43,32 +45,41 @@ GPIO.setup(SERVO_GPIO, GPIO.OUT)
 ###### 鍵の設定 #######
 # 鍵の状態を保管w
 status = ""
+db_change_signal = "off"
 ####################		
 
 
 def nfc_imput():
-	# USBに接続されたNFCリーダに接続してインスタンス化
-	clf = nfc.ContactlessFrontend('usb')
-	while True:
+    global db_change_signal
+    # USBに接続されたNFCリーダに接続してインスタンス化
+    clf = nfc.ContactlessFrontend('usb')
+    while True:
         # Suica待ち受け開始
         # clf.sense( [リモートターゲット], [検索回数], [検索の間隔] )
-		target_res = clf.sense(target_req_suica, iterations=int(TIME_cycle//TIME_interval)+1 , interval=TIME_interval)
-		if target_res is not None: 
+        target_res = clf.sense(target_req_suica, iterations=int(TIME_cycle//TIME_interval)+1 , interval=TIME_interval)
+        if target_res is not None: 
             #tag = nfc.tag.tt3.Type3Tag(clf, target_res)
             #なんか仕様変わったっぽい？↓なら動いた
-			tag = nfc.tag.activate_tt3(clf, target_res)
-			tag.sys = 3
-			#IDmを取り出す
-			idm = binascii.hexlify(tag.idm)
-			#print('Suica detected. idm = ' + str(idm))
-			break
-	clf.close()	
-	return idm
+            tag = nfc.tag.activate_tt3(clf, target_res)
+            tag.sys = 3
+            #IDmを取り出す
+            idm = binascii.hexlify(tag.idm)
+            #print('Suica detected. idm = ' + str(idm))
+            break
+        if db_change_signal == "on":
+            idm = "db_change"
+            break
+        
+        
+    clf.close()	
+    return idm
 
 
-def database_change():
-    print("NFC登録データベースを改変します。以下の選択肢から該当番号を入力してください。\n データの追加:1\n データの削除:2\n 全データの照会:3")
-    number = input('該当番号>> ')
+def database_change(waitinput):
+    global db_change_signal
+    #print("NFC登録データベースを改変します。以下の選択肢から該当番号を入力してください。\n データの追加:1\n データの削除:2\n 全データの照会:3")
+    number = waitinput
+    #number = input('該当番号>> ')
 
     # データの追加
     if number == str(1):
@@ -76,6 +87,7 @@ def database_change():
         print("あなたの名前を教えてください。 例　Shintaro_Hasegawa")
         your_name = input('name>> ')
         print("name : " + your_name)
+        db_change_signal = "off"
         print("登録するNFCカードをリーダーにかざしてください")
         idm = nfc_imput()
 
@@ -88,17 +100,18 @@ def database_change():
         # idmがデータベースになかったら
         if serchresult == None:
             databese_namelist.insert(person_name, idm_number, detect_time)
-            print("registered your name and nfc card.")
+            print(">>> registered your name and nfc card.")
         # idmがデータベースにあったら
         else:
             databese_namelist.update(person_name, idm_number, detect_time)
             print(serchresult[0] + "　　>>　　" + person_name)
-            print("database was updated.")
+            print(">>> database was updated.")
 
 
     # データの削除
     elif number == str(2):
         print("登録データの削除をします．")
+        db_change_signal = "off"
         print("削除するNFCカードをリーダーにかざしてください")
         idm_number = str(nfc_imput())
         # データベースにidm_numberがあるかどうか検索
@@ -106,21 +119,26 @@ def database_change():
         #　idmがデータベースにあったら
         if serchresult is not None:
             databese_namelist.delite(idm_number)
-            print("complited delete data")
+            print(">>> Data deletion completed")
         #  idmがデータベースになかったら   
         else:
-            print("登録されていません")
+            print(">>> このNFCカードは登録されていません")
 
 
     # データの表示
     elif number == str(3):
         print("登録されているIDの一覧を表示します．")
+        db_change_signal = "off"
         datas = databese_namelist.show()
         for data in datas:
             print(data)
     else:
         print("errer")
-        print("errer ¥n 半角で　1 or 2 or 3を入力して")
+        print("半角で　1 or 2 or 3を入力して")
+    
+    time.sleep(5)
+    
+
 
 
 class Databese_Namelist():
@@ -184,15 +202,128 @@ class Databese_Namelist():
 
 
 
+# サーボ関係
+class ServoCon:
+
+    def control_key(dc):
+        servo = GPIO.PWM(SERVO_GPIO, SERVO_PWM)
+        servo.start(0.0)
+        servo.ChangeDutyCycle(dc)
+        time.sleep(0.3) #モータが動き終わるまで待ち
+        servo.stop()
+
+
+    def open_key():
+        global status
+        ServoCon.control_key(SERVO_OPEN)
+        time.sleep(0.5)       
+        ServoCon.control_key(SERVO_wait)
+        time.sleep(0.5) 
+        status = "open"
+        return status
+        servo.stop()
+
+
+    def close_key():
+        global status
+        ServoCon.control_key(SERVO_CLOSE)
+        time.sleep(0.5)       
+        ServoCon.control_key(SERVO_wait)
+        time.sleep(0.5)
+        status = "close"
+        return status
+        servo.stop()
+
+
+    def get_status():
+        global status
+        return status
+        servo.stop()
+
+def nfcservo():
+    global imput_number
+
+    print ('NFC waiting...')
+    idm = nfc_imput()
+    if idm == "db_change":
+        database_change(imput_number)
+
+        return
+    # 特定のIDmだった場合のアクション
+    # データベースにidm_numberがあるかどうか検索
+    serchresult = databese_namelist.idm_serch(str(idm))
+    # idmがデータベースになかったら
+    if serchresult == None:
+        print(str(idm) + "は登録されていません")
+        # logを記録する
+
+        time.sleep(1)
+        
+    elif serchresult[1] == str(idm):
+        print("【 " + serchresult[0] + "の登録済みid : " + serchresult[1] + "】")
+        
+        # 現在の状態を記録
+        before_status = ServoCon.get_status()
+        
+        if before_status == "open":
+            ServoCon.close_key()
+        elif before_status == "close":
+            ServoCon.open_key()
+        print("status : " + before_status + ">>>>>" + ServoCon.get_status())
+        
+        # logを記録する
+        #file = open(file_log,'a')
+        #file.write(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ", " + result[0]+ ", " + str(idm) + ", " + before_status + ">>>" + ServoCon.get_status() + "\n")
+        #file.close()
+
+        # 念のため１秒まつ
+        time.sleep(1)    
+        
+    else:
+        print("errer")
+
+def waitcount():
+    global db_change_signal
+    global imput_number
+    print("NFC登録データベースを改変します。以下の選択肢から該当番号を入力してください。\n データの追加:1\n データの削除:2\n 全データの照会:3")
+    number = input('該当番号>> ')
+    if number == str(1) or str(2) or str(3):
+        db_change_signal = "on"
+        #print(db_change_signal)
+        imput_number = number
+        
+    else:
+        print("errer")
+        print("半角で　1 or 2 or 3を入力して")
+        
+        
     
 if __name__ == '__main__':
     try:
+        # 最初にオープン状態にする
+        ServoCon.control_key(SERVO_OPEN)
+        status = "open"
         databese_namelist = Databese_Namelist()
-        #while 1:
-            
-        database_change()
+
+        
+        # logを記録する
+        #file = open(file_log,'a')
+        #file.write(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ", nfcservo.py_START\n")
+        #file.close()
+        
+        while True:
+            #nfcservo_thread = threading.Thread(target=nfcservo)
+            waitcount_thread = threading.Thread(target=waitcount)
+
+            #nfcservo_thread.start()
+            waitcount_thread.start()
+            #nfcservo_thread.join()
+            #waitcount_thread.join()
+
+            nfcservo()
 
     except KeyboardInterrupt:
-        
-        cconnection.close()
+        GPIO.cleanup()
+        waitcount_thread.kill()
+        #cconnection.close()
         pass
